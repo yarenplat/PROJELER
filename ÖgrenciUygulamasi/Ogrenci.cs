8da9d4ba718a2bad6156a48a708714dee308e9b0
yaren <yarenplat@gmail.com>
2023-03-08 21:54:58 -0800

    Ogrenci Uygulamasi

---
+++ ÖgrenciUygulamasi/Ogrenci.cs
@@ -0,0 +1,42 @@
+﻿namespace OgrenciUygulamasi;
+
+public class Ogrenci
+{
+    public int ogrenciNo;
+    private string isim;
+    private string soyisim;
+    private int vize1;
+    private int vize2;
+    private int final;
+    private string okulİsmi;
+
+    public Ogrenci(int _ogrenciNo, string _isim, string _soyisim, int _vize1, int _vize2, int _final, string _okulİsmi)
+    {
+        ogrenciNo = _ogrenciNo;
+        isim = _isim;
+        soyisim = _soyisim;
+        vize1 = _vize1;
+        vize2 = _vize2;
+        final = _final;
+        okulİsmi = _okulİsmi;
+    }
+    public void ogrenciBilgileriGoster()
+    {
+        Console.WriteLine("Ögrenci Numarası:" + ogrenciNo);
+        Console.WriteLine("Öğrenci İsmi:" + isim);
+        Console.WriteLine("Öğrenci Soyismi:" + soyisim);
+        Console.WriteLine("Öğrenci Birinci Vize Notu:" + vize1);
+        Console.WriteLine("Öğrenci İkinic Vize Notu:" + vize2);
+        Console.WriteLine("Öğrenci Final Notu:" + vize1);
+        Console.WriteLine("Öğrenci Okul İsmi:" + okulİsmi);
+    }
+    public double ogrenciNotuBul()
+    {
+        double ortalama = vize1 * 0.2 + vize2 * 0.2 + final * 0.6;
+        return ortalama;
+    }
+    public void okulgetir()
+    {
+        Console.WriteLine("Okul İsmi:" + okulİsmi);
+    }
+}
---
+++ ÖgrenciUygulamasi/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace OgrenciUygulamasi;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        bool kontrol = true;
+
+        Ogrenci ogrenci1 = new Ogrenci(14, "Yaren", "Polat", 90, 90, 90, "GKAL");
+
+        while (kontrol)
+        {
+            İslemleriGöster();
+            string secim = Console.ReadLine();
+
+            switch (secim)
+            {
+                case "1":
+                    ogrenci1.ogrenciBilgileriGoster();
+                    break;
+
+                case "2":
+                    double ogrenciOrtalaması = ogrenci1.ogrenciNotuBul();
+                    Console.WriteLine("Öğrenci Ortalaması" + ogrenciOrtalaması);
+                    break;
+
+                case "3":
+                    ogrenci1.okulgetir();
+                    break;
+                case "4":
+                    kontrol = false;
+                    break;
+
+            }
+        }
+
+    }
+
+
+
+
+    static void İslemleriGöster()
+    {
+        Console.WriteLine("1-Öğrenci Bilgilerini Göster");
+        Console.WriteLine("2-Öğrenci Ortalamasını Göster");
+        Console.WriteLine("3-Öğrenci Okul Adını Göster");
+        Console.WriteLine("4-Çıkış");
+
+
+    }
+
+}
+
