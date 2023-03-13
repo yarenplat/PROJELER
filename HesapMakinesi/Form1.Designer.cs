447a6f0b087cae19cd935a01fae81ebe2aab623d
yaren <yarenplat@gmail.com>
2023-03-12 20:24:16 -0700

    .

---
+++ HesapMakinesi/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace HesapMakinesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEkran = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnArti = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnEsittir = new System.Windows.Forms.Button();
+            this.btnEksi = new System.Windows.Forms.Button();
+            this.btnCarpma = new System.Windows.Forms.Button();
+            this.btnBolme = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEkran
+            // 
+            this.lblEkran.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblEkran.BackColor = System.Drawing.SystemColors.Window;
+            this.lblEkran.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblEkran.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblEkran.Location = new System.Drawing.Point(12, 9);
+            this.lblEkran.Name = "lblEkran";
+            this.lblEkran.Size = new System.Drawing.Size(295, 45);
+            this.lblEkran.TabIndex = 0;
+            this.lblEkran.Text = "0";
+            this.lblEkran.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.LightCoral;
+            this.btn1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn1.Location = new System.Drawing.Point(12, 68);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(70, 70);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = false;
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.LightCoral;
+            this.btn2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn2.Location = new System.Drawing.Point(88, 68);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(70, 70);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightCoral;
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.Location = new System.Drawing.Point(164, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 70);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "1";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.LightCoral;
+            this.btn4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn4.Location = new System.Drawing.Point(12, 144);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(70, 70);
+            this.btn4.TabIndex = 1;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = false;
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.LightCoral;
+            this.btn3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn3.Location = new System.Drawing.Point(164, 68);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(70, 70);
+            this.btn3.TabIndex = 1;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = false;
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.LightCoral;
+            this.btn5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn5.Location = new System.Drawing.Point(88, 144);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(70, 70);
+            this.btn5.TabIndex = 1;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = false;
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.LightCoral;
+            this.btn6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn6.Location = new System.Drawing.Point(164, 144);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(70, 70);
+            this.btn6.TabIndex = 1;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = false;
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.LightCoral;
+            this.btn8.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn8.Location = new System.Drawing.Point(88, 220);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(70, 70);
+            this.btn8.TabIndex = 1;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = false;
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.LightCoral;
+            this.btn7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn7.Location = new System.Drawing.Point(12, 220);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(70, 70);
+            this.btn7.TabIndex = 1;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = false;
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.LightCoral;
+            this.btn9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn9.Location = new System.Drawing.Point(164, 220);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(70, 70);
+            this.btn9.TabIndex = 1;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = false;
+            // 
+            // btnArti
+            // 
+            this.btnArti.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnArti.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnArti.Location = new System.Drawing.Point(235, 68);
+            this.btnArti.Name = "btnArti";
+            this.btnArti.Size = new System.Drawing.Size(70, 70);
+            this.btnArti.TabIndex = 1;
+            this.btnArti.Text = "+";
+            this.btnArti.UseVisualStyleBackColor = false;
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(0, 0);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(75, 23);
+            this.btn0.TabIndex = 0;
+            // 
+            // btnC
+            // 
+            this.btnC.Location = new System.Drawing.Point(0, 0);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(75, 23);
+            this.btnC.TabIndex = 0;
+            // 
+            // btnEsittir
+            // 
+            this.btnEsittir.Location = new System.Drawing.Point(0, 0);
+            this.btnEsittir.Name = "btnEsittir";
+            this.btnEsittir.Size = new System.Drawing.Size(75, 23);
+            this.btnEsittir.TabIndex = 0;
+            // 
+            // btnEksi
+            // 
+            this.btnEksi.Location = new System.Drawing.Point(0, 0);
+            this.btnEksi.Name = "btnEksi";
+            this.btnEksi.Size = new System.Drawing.Size(75, 23);
+            this.btnEksi.TabIndex = 0;
+            // 
+            // btnCarpma
+            // 
+            this.btnCarpma.Location = new System.Drawing.Point(0, 0);
+            this.btnCarpma.Name = "btnCarpma";
+            this.btnCarpma.Size = new System.Drawing.Size(75, 23);
+            this.btnCarpma.TabIndex = 0;
+            // 
+            // btnBolme
+            // 
+            this.btnBolme.Location = new System.Drawing.Point(0, 0);
+            this.btnBolme.Name = "btnBolme";
+            this.btnBolme.Size = new System.Drawing.Size(75, 23);
+            this.btnBolme.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(443, 358);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label lblEkran;
+        private Button btn1;
+        private Button btn2;
+        private Button button3;
+        private Button btn4;
+        private Button btn3;
+        private Button btn5;
+        private Button btn6;
+        private Button btn8;
+        private Button btn7;
+        private Button btn9;
+        private Button btnArti;
+        private Button btn0;
+        private Button btnC;
+        private Button btnEsittir;
+        private Button btnEksi;
+        private Button btnCarpma;
+        private Button btnBolme;
+    }
+}
---
+++ HesapMakinesi/Form1.cs
@@ -0,0 +1,12 @@
+namespace HesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
