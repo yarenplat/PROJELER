593a055582d1ed2c9a4572849fbf30f20bf2ae66
yaren <yarenplat@gmail.com>
2023-03-13 21:14:20 -0700

    .

--- CafeSiparis/BAGY.Designer.cs
+++ CafeSiparis/BAGY.Designer.cs
@@ -48,6 +48,7 @@
             // 
             // gbxSiparis
             // 
+            this.gbxSiparis.BackColor = System.Drawing.Color.LightSalmon;
             this.gbxSiparis.Controls.Add(this.btnGE);
             this.gbxSiparis.Controls.Add(this.btnGA);
             this.gbxSiparis.Controls.Add(this.btnKE);
@@ -62,59 +63,63 @@
             this.gbxSiparis.Controls.Add(this.lblCay);
             this.gbxSiparis.Location = new System.Drawing.Point(12, 12);
             this.gbxSiparis.Name = "gbxSiparis";
-            this.gbxSiparis.Size = new System.Drawing.Size(488, 293);
+            this.gbxSiparis.Size = new System.Drawing.Size(390, 276);
             this.gbxSiparis.TabIndex = 0;
             this.gbxSiparis.TabStop = false;
             this.gbxSiparis.Text = "Sipariş";
             // 
             // btnGE
             // 
+            this.btnGE.BackColor = System.Drawing.Color.Thistle;
             this.btnGE.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnGE.Location = new System.Drawing.Point(399, 181);
+            this.btnGE.Location = new System.Drawing.Point(284, 184);
             this.btnGE.Name = "btnGE";
             this.btnGE.Size = new System.Drawing.Size(74, 72);
             this.btnGE.TabIndex = 2;
             this.btnGE.Text = "-";
-            this.btnGE.UseVisualStyleBackColor = true;
+            this.btnGE.UseVisualStyleBackColor = false;
             this.btnGE.Click += new System.EventHandler(this.btnGE_Click);
             // 
             // btnGA
             // 
+            this.btnGA.BackColor = System.Drawing.Color.Purple;
             this.btnGA.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnGA.Location = new System.Drawing.Point(319, 181);
+            this.btnGA.Location = new System.Drawing.Point(204, 184);
             this.btnGA.Name = "btnGA";
             this.btnGA.Size = new System.Drawing.Size(74, 72);
             this.btnGA.TabIndex = 2;
             this.btnGA.Text = "+";
-            this.btnGA.UseVisualStyleBackColor = true;
+            this.btnGA.UseVisualStyleBackColor = false;
             this.btnGA.Click += new System.EventHandler(this.btnGA_Click);
             // 
             // btnKE
             // 
+            this.btnKE.BackColor = System.Drawing.Color.Thistle;
             this.btnKE.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnKE.Location = new System.Drawing.Point(399, 111);
+            this.btnKE.Location = new System.Drawing.Point(284, 114);
             this.btnKE.Name = "btnKE";
             this.btnKE.Size = new System.Drawing.Size(74, 67);
             this.btnKE.TabIndex = 2;
             this.btnKE.Text = "-";
-            this.btnKE.UseVisualStyleBackColor = true;
+            this.btnKE.UseVisualStyleBackColor = false;
             this.btnKE.Click += new System.EventHandler(this.btnKE_Click);
             // 
             // btnKA
             // 
+            this.btnKA.BackColor = System.Drawing.Color.Purple;
             this.btnKA.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnKA.Location = new System.Drawing.Point(319, 111);
+            this.btnKA.Location = new System.Drawing.Point(204, 114);
             this.btnKA.Name = "btnKA";
             this.btnKA.Size = new System.Drawing.Size(74, 67);
             this.btnKA.TabIndex = 2;
             this.btnKA.Text = "+";
-            this.btnKA.UseVisualStyleBackColor = true;
+            this.btnKA.UseVisualStyleBackColor = false;
             this.btnKA.Click += new System.EventHandler(this.btnKA_Click);
             // 
             // lblGazozSayisi
             // 
             this.lblGazozSayisi.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblGazozSayisi.Location = new System.Drawing.Point(115, 192);
+            this.lblGazozSayisi.Location = new System.Drawing.Point(139, 179);
             this.lblGazozSayisi.Name = "lblGazozSayisi";
             this.lblGazozSayisi.Size = new System.Drawing.Size(55, 46);
             this.lblGazozSayisi.TabIndex = 1;
@@ -122,19 +127,20 @@
             // 
             // btnCE
             // 
+            this.btnCE.BackColor = System.Drawing.Color.Thistle;
             this.btnCE.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnCE.Location = new System.Drawing.Point(399, 37);
+            this.btnCE.Location = new System.Drawing.Point(284, 40);
             this.btnCE.Name = "btnCE";
             this.btnCE.Size = new System.Drawing.Size(74, 71);
             this.btnCE.TabIndex = 2;
             this.btnCE.Text = "-";
-            this.btnCE.UseVisualStyleBackColor = true;
+            this.btnCE.UseVisualStyleBackColor = false;
             this.btnCE.Click += new System.EventHandler(this.btnCE_Click);
             // 
             // lblKahveSayisi
             // 
             this.lblKahveSayisi.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblKahveSayisi.Location = new System.Drawing.Point(115, 127);
+            this.lblKahveSayisi.Location = new System.Drawing.Point(139, 114);
             this.lblKahveSayisi.Name = "lblKahveSayisi";
             this.lblKahveSayisi.Size = new System.Drawing.Size(42, 57);
             this.lblKahveSayisi.TabIndex = 1;
@@ -143,7 +149,7 @@
             // lblGazoz
             // 
             this.lblGazoz.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblGazoz.Location = new System.Drawing.Point(0, 187);
+            this.lblGazoz.Location = new System.Drawing.Point(6, 174);
             this.lblGazoz.Name = "lblGazoz";
             this.lblGazoz.Size = new System.Drawing.Size(94, 51);
             this.lblGazoz.TabIndex = 0;
@@ -151,19 +157,20 @@
             // 
             // btnCA
             // 
+            this.btnCA.BackColor = System.Drawing.Color.Purple;
             this.btnCA.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnCA.Location = new System.Drawing.Point(319, 37);
+            this.btnCA.Location = new System.Drawing.Point(204, 40);
             this.btnCA.Name = "btnCA";
             this.btnCA.Size = new System.Drawing.Size(74, 71);
             this.btnCA.TabIndex = 2;
             this.btnCA.Text = "+";
-            this.btnCA.UseVisualStyleBackColor = true;
+            this.btnCA.UseVisualStyleBackColor = false;
             this.btnCA.Click += new System.EventHandler(this.btnCA_Click);
             // 
             // lblKahve
             // 
             this.lblKahve.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblKahve.Location = new System.Drawing.Point(0, 123);
+            this.lblKahve.Location = new System.Drawing.Point(6, 116);
             this.lblKahve.Name = "lblKahve";
             this.lblKahve.Size = new System.Drawing.Size(100, 58);
             this.lblKahve.TabIndex = 0;
@@ -172,7 +179,7 @@
             // lblCaySayisi
             // 
             this.lblCaySayisi.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblCaySayisi.Location = new System.Drawing.Point(115, 63);
+            this.lblCaySayisi.Location = new System.Drawing.Point(139, 50);
             this.lblCaySayisi.Name = "lblCaySayisi";
             this.lblCaySayisi.Size = new System.Drawing.Size(51, 53);
             this.lblCaySayisi.TabIndex = 1;
@@ -181,7 +188,7 @@
             // lblCay
             // 
             this.lblCay.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblCay.Location = new System.Drawing.Point(6, 62);
+            this.lblCay.Location = new System.Drawing.Point(6, 58);
             this.lblCay.Name = "lblCay";
             this.lblCay.Size = new System.Drawing.Size(94, 54);
             this.lblCay.TabIndex = 0;
@@ -190,34 +197,37 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 311);
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(12, 294);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(235, 64);
+            this.button1.Size = new System.Drawing.Size(181, 64);
             this.button1.TabIndex = 1;
             this.button1.Text = "SIFIRLA";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(253, 311);
+            this.button2.BackColor = System.Drawing.Color.LawnGreen;
+            this.button2.Location = new System.Drawing.Point(216, 294);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(247, 64);
+            this.button2.Size = new System.Drawing.Size(186, 64);
             this.button2.TabIndex = 1;
             this.button2.Text = "SİPARİŞİ TAMAMLA";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // BAGY
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(512, 436);
+            this.BackColor = System.Drawing.Color.Tomato;
+            this.ClientSize = new System.Drawing.Size(420, 407);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.gbxSiparis);
             this.Name = "BAGY";
-            this.Text = "Form1";
+            this.Text = "BaGY Çay Bahçesi";
             this.gbxSiparis.ResumeLayout(false);
             this.ResumeLayout(false);
 
